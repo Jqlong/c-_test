d7f65f340852836af57b66ca55b8fa526a668fb4
JqlongC <2235715529@qq.com>
2022-03-03 14:14:45 +0800

    main

---
+++ c++学习/c++学习/constant.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//using namespace std;
+//
+////常量的定义
+////1、#define宏常量
+////2.const修饰的变量
+//
+//
+//#define DAY 7
+//int main() {
+//
+//	//cout << "hello world" << endl;
+//
+//	cout << "一周有：" << DAY << "天" << endl;
+//
+//	const int month = 12;
+//	//month = 13;//const修饰的变量也称为常量；
+//
+//	cout << "一年的月份：" << month << endl;
+//
+//	system("pause");
+//}
---
+++ c++学习/c++学习/关键字.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	//不要用关键字给变量或者常量起名称
+	//int int = 1;//int是一个关键字，不可以作为变量的名称
+
+	int num1 = 10;
+	int num2 = 20;
+	cout << num1 + num2 << endl;
+	system("pause");
+}
